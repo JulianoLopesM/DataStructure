d3ec782fa2a28ae533c214e10214283e60f98d29
JulianoLopesM <135735980+JulianoLopesM@users.noreply.github.com>
2023-09-20 22:59:52 -0300

    Explicando referencia na memoria para objetos e tipos primitivos

---
+++ src/com/projeto/atribuicaoreferencia/Main.java
@@ -0,0 +1,20 @@
+package com.projeto.atribuicaoreferencia;
+
+public class Main {
+    public static void main(String[] args) {
+        int intA = 1;
+        int intB = intA;
+        System.out.println( "int a= " + intA +  " e " +"int b = " + intB);
+        intA = 3;
+        System.out.println(intA);
+        System.out.println( "int a= " + intA +  " e " +"int b = " + intB);
+
+        MeuObj obj1 = new MeuObj(1);
+        MeuObj obj2 = obj1;
+        System.out.println("obj1 = " + obj1 + " obj2 = " + obj2);
+        obj1.setNum(3);
+        System.out.println("obj1 = " + obj1 + " obj2 = " + obj2);
+
+
+    }
+}
